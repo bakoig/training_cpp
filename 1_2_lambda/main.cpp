546651d2880fff5b5156189d911ae608ec6aa224
Kanstantsin Balabanau <kanstantsin_balabanau@epam.com>
2022-02-11 11:22:45 +0300

    1_2_lambda

--- 1_2_lambda/main.cpp
+++ 1_2_lambda/main.cpp
@@ -7,11 +7,14 @@ using namespace std;
 int main(int argc, char** argv)
 {
     vector<int> vec{1,3,5,7,9,2,4,6,8,0};
-    auto print = [](int x) { cout << x << endl; };
+    auto print = [](int x) { cout << x << " "; };
     for_each(vec.begin(), vec.end(), print);
+    cout << endl;
 
-    sort(vec.begin(), vec.end(), [](int x, int y) { return x < y; });
+    int cnt = 0;
+    sort(vec.begin(), vec.end(), [&cnt](int x, int y) { if (x < y) {cnt++;}; return x < y; });
     for_each(vec.begin(), vec.end(), print);
+    cout << cnt << endl;
 
     return 0;
 }
